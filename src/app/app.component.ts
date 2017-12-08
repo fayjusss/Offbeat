562e46d6c6086f669cb43e71ae0b7eb94e369afa
Unknown <gaelle.cudennec@gmail.com>
2017-12-08 08:20:51 +0200

    Adding Learn page / Study diary

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { HomePage } from '../pages/home/home';
+import { LearnPage } from '../pages/learn/learn';
 @Component({
   templateUrl: 'app.html'
 })
@@ -19,4 +20,3 @@ export class MyApp {
     });
   }
 }
-
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { LearnPage } from '../pages/learn/learn';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    LearnPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    LearnPage
   ],
   providers: [
     StatusBar,
